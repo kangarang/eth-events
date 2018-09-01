b6f83987a0fba2f19499910bfae35b9c17821d9a
Isaac Kang <isaac.kang@consensys.net>
2018-08-31 21:36:16 -0400

    add parcel-bundler. add counter

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const actions = {
 
 const view = (state, actions) => (
   <div>
+    eth-events
     <h1>{state.count}</h1>
     <button onclick={() => actions.down(1)}>-</button>
     <button onclick={() => actions.up(1)}>+</button>
