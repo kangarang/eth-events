f76f115840023b18d7b3b489397f1fc8a9d4610f
Isaac Kang <isaac.kang@consensys.net>
2018-09-11 13:13:12 -0400

    rm test

--- test/EthEvents.js
+++ test/EthEvents.js
@@ -9,9 +9,9 @@ const {
 test('EthEvents.getLogs - Transfer', async t => {
   try {
     const contract = buildContract('adChain', 'token')
-    const ethEvents = new EthEvents(contract, 5000)
+    const ethEvents = new EthEvents(contract, 10000)
 
-    const fromBlock = 6270678
+    const fromBlock = 6300000
     const toBlock = 'latest'
     const eventNames = ['Transfer']
     const indexedFilterValues = {
@@ -27,35 +27,4 @@ test('EthEvents.getLogs - Transfer', async t => {
   } catch (error) {
     console.log('ddd error:', error)
   }
-})
-
-test('EthEvents.getLogs - Approval', async t => {
-  const contract = buildContract('adChain', 'token')
-  const ethEvents = new EthEvents(contract, 5000)
-
-  const fromBlock = 6150000
-  const toBlock = 'latest'
-  const eventNames = ['Approval']
-
-  const logs = await ethEvents.getLogs(fromBlock, toBlock, eventNames, {}, true)
-
-  t.notEqual(logs.length, 0, 'should have length')
-  t.end()
-})
-
-test('EthEvents.getLogs - Transfer to 0xb4b26709f...', async t => {
-  const contract = buildContract('adChain', 'token')
-  const ethEvents = new EthEvents(contract, 5000)
-
-  const fromBlock = 6150000
-  const toBlock = 'latest'
-  const eventNames = ['Transfer']
-  const indexedFilterValues = {
-    _to: '0xb4b26709ffed2cd165b9b49eea1ac38d133d7975',
-  }
-
-  const logs = await ethEvents.getLogs(fromBlock, toBlock, eventNames, indexedFilterValues)
-
-  t.notEqual(logs.length, 0, 'should have length')
-  t.end()
 })
