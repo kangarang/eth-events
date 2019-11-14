299cee68f9eaa33cbbbd6eb6970613817e137ee7
Isaac Kang <isaac.kang@consensys.net>
2019-11-14 17:15:35 -0500

    Updates

---
+++ precache-manifest.518287de8a4059cb1339da8e6224e751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973bab7ac7424c61b335e7d061e7d94a",
+    "url": "/eth-events/index.html"
+  },
+  {
+    "revision": "dfc93f11541d356e9be8",
+    "url": "/eth-events/static/css/main.306fe3b5.chunk.css"
+  },
+  {
+    "revision": "92bdea36d26322b3fe38",
+    "url": "/eth-events/static/js/2.6c89f4d7.chunk.js"
+  },
+  {
+    "revision": "dfc93f11541d356e9be8",
+    "url": "/eth-events/static/js/main.f48f12e1.chunk.js"
+  },
+  {
+    "revision": "992f12bc36e22c9133c9",
+    "url": "/eth-events/static/js/runtime-main.1e5f901b.js"
+  }
+]);
